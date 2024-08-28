2ded8b90183fe00f4be6a579918429adff9c324e
Ali Muhammad <ali9796408@gmail.com>
2024-08-29 03:37:37 +0500

    layout fix

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function Login() {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div className="w-full lg:grid lg:grid-cols-2 min-h-screen">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
